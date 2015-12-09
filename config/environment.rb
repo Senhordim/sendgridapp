d1654d7f16db981261df648ca51d4b75be3176ae
Diego Collares <diego.collares@gmail.com>
2015-12-09 19:36:56 -0200

    set user e password

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'your_sendgrid_username',
-  :password => 'your_sendgrid_password',
+  :user_name => 'senhordim',
+  :password => 'kika1984*',
   :domain => 'sendemailapp2.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
